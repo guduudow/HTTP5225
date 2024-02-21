ba40f9f17dd9e93b276617470ffb301fbf3e3044
guduudow <bbo3063@gmail.com>
2024-02-21 18:56:33 -0500

    Re-added important things
    Added the if statement in order to hide the final match category for those who are still playing, added the form buttons to edit and delete a player.

--- php-assignments/assignment1/index.php
+++ php-assignments/assignment1/index.php
@@ -62,18 +62,25 @@ GROUP BY
     <div class="row">
         <?php
         foreach ($players as $row) {
+
+          if($row['final_match']!= null ){
+            $finalgame = "<p class='card-text'>Final: ". $row['final_match'] ."</p>";
+          } else {
+            $finalgame = "";
+          }
+
             echo '<div class="col-md-4 mb-4">
                     <div class="card h-100">
-                        <img src="' . $row['imageURL'] . '" class="card-img-top" alt="Player Image">
+                        <img src="' . $row['imageURL'] . '" class="card-img-top" alt="Player Image" style="height: auto;">
                         <div class="card-body">
                             <h5 class="card-title">' . $row['player_name'] . '</h5>
-                            <p class="card-text">Position:' . $row['position'] . '</p>
-                            <p class="card-text">Matches Played:' . $row['matches_played'] . '</p>
-                            <p class="card-text">Goals:' . $row['goals'] . '</p>
-                            <p class="card-text">Assists:' . $row['assists'] . '</p>
-                            <p class="card-text">Debut:' . $row['debut'] . '</p>
-                            <p class="card-text">Final Match:' . $row['final_match'] . '</p>
-                            <p class="card-text">Country:' . $row['country'] . '</p>
+                            <p class="card-text">Position: ' . $row['position'] . '</p>
+                            <p class="card-text">Matches Played: ' . $row['matches_played'] . '</p>
+                            <p class="card-text">Goals: ' . $row['goals'] . '</p>
+                            <p class="card-text">Assists: ' . $row['assists'] . '</p>
+                            <p class="card-text">Debut: ' . $row['debut'] . '</p>
+                            ' . $finalgame . '
+                            <p class="card-text">Country: ' . $row['country'] . '</p>
                             <div class="mt-3">
                                 <h6>Teams:</h6>';
 
@@ -84,6 +91,16 @@ GROUP BY
 
             // Close the HTML tags
             echo '</div>
+                  <div class="col-lg-2 d-flex align-items-center">
+                    <form method="GET" action="update.php">
+                      <input type="hidden" name="playerid" value="' . $row['playerid'] .'">
+                      <button type="submit" name="edit" class="btn btn-sm btn-info my-5 mx-5">Edit</button>
+                    </form>
+                    <form method="GET" action="includes/deletePlayer.php">
+                        <input type="hidden" name="playerid" value="' . $row['playerid'] .'">
+                        <button type="submit" name="delete" class="btn btn-sm btn-danger my-5 mx-5">Delete</button>
+                    </form>
+                  </div>
                     </div>
                 </div>
             </div>';
